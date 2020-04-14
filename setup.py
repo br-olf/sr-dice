6bb11271cfc103442e4c599a2bc0331f96d1d2ed
Brolf <brolf@magheute.net>
2020-04-14 14:25:06 +0200

    fixed license classifier

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: EUPL-1.2",
+        "License :: OSI Approved :: European Union Public Licence 1.2 (EUPL 1.2)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
