d11ad3a661e3387ef79d404b5dfbf7e3599a8a03
Brolf <brolf@magheute.net>
2020-04-14 14:22:00 +0200

    fixed typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/br-olf/sr-dice",
-    keywords=['dice','pen and paper','RPG','Shadowrun']
+    keywords=['dice','pen and paper','RPG','Shadowrun'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
